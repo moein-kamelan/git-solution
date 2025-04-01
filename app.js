87f84444fd72fb3f05d105209140f653124f39af
Moein Kamelan <164170938+moein-kamelan@users.noreply.github.com>
2025-04-01 11:21:24 +0330

    Update app.js
    remove and add codes in app.js

--- app.js
+++ app.js
@@ -3,18 +3,6 @@ return "hello"
 }
 console.log(sayHello())
 
-const sayDevelopeRemote = () => {
-return "developeRemote"
-}
-;
-const sayDeveloper = () => {
-  return "i'm a developer"
-}
-
-const sayFriend = () => {
-    return "my friend is ali"
-}
-
-console.log(sayFriend());
+const myAge = 18
 
 
